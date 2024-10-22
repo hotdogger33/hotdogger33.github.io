daa8aff4661ced4dbe2f3664edb609a4b73effb8
hotdogger33 <jacobschwartzmail@gmail.com>
2024-10-22 16:21:45 -0400

    creation
    when i first created the github repository

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const menu = document.getElementById("vertical-bar");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
